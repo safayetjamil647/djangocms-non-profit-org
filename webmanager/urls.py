9960ed8513b9f7cf574bf78d40ed18dde39a1c7c
safayetjamil647 <work.jamil4@gmail.com>
2020-05-15 03:20:06 +0600

    login system

---
+++ webmanager/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.home, name="home"),
+
+    path('contact', views.contact, name="contact"),
+    path('login', views.loginPage, name="login"),
+    path('register', views.register, name="register"),
+    path('logout', views.logoutUser, name="logout"),
+
+
+]
---
+++ webmanager/views.py
@@ -0,0 +1,58 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from .forms import CreateUserForm
+
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.http import HttpResponse
+
+
+# Create your views here.
+
+
+def home(request):
+    return render(request, 'webmanager/home.html')
+
+
+def contact(request):
+    return render(request, 'webmanager/contact.html')
+
+
+def loginPage(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.info(request, 'Username OR password is incorrect')
+        context = {}
+        return render(request, 'webmanager/login.html', context)
+
+
+def register(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        form = CreateUserForm()
+
+        if request.method == 'POST':
+            form = UserCreationForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                messages.success(request, 'Account created for ' + user)
+                return redirect('login')
+        context = {'form': form}
+        return render(request, 'webmanager/register.html', context)
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
+
