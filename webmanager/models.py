575ef92e0a7cd408199ba7e9f6d14b1080829e98
safayetjamil647 <work.jamil4@gmail.com>
2020-05-31 01:02:36 +0600

    v0.04

--- webmanager/models.py
+++ webmanager/models.py
@@ -6,6 +6,8 @@ class Blog(models.Model):
     blog_title = models.CharField(max_length=100)
     blog_desc = models.TextField(max_length=1000)
     profile_pic = models.ImageField(null=True, blank=True)
+    created_at = models.DateTimeField(null=True, blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.blog_title
@@ -15,6 +17,8 @@ class Appeal(models.Model):
     appeal_title = models.CharField(max_length=100)
     appeal_desc = models.TextField(max_length=1000)
     appeal_pic = models.ImageField(null=True, blank=True)
+    created_at = models.DateTimeField(null=True, blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.appeal_title
@@ -24,6 +28,8 @@ class Sponsor(models.Model):
     sponsor_title = models.CharField(max_length=100)
     sponsor_desc = models.TextField(max_length=1000)
     sponsor_pic = models.ImageField(null=True, blank=True)
+    created_at = models.DateTimeField(null=True, blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.sponsor_title
@@ -33,6 +39,8 @@ class Event(models.Model):
     event_title = models.CharField(max_length=100)
     event_desc = models.TextField(max_length=1000)
     event_pic = models.ImageField(null=True, blank=True)
+    created_at = models.DateTimeField(null=True, blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.event_title
@@ -41,6 +49,8 @@ class Event(models.Model):
 class Gallery(models.Model):
     image_desc = models.TextField(max_length=200)
     image_pic = models.ImageField(null=True, blank=True)
+    created_at = models.DateTimeField(null=True, blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.image_desc
